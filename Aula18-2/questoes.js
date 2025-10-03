e22672cdbed12e178cd1a4e468c657067d33983b
Rodrigo <2rodrigosilva2@gmail.com>
2025-10-03 09:57:04 -0300

    questões

--- Aula18-2/questoes.js
+++ Aula18-2/questoes.js
@@ -1,50 +1,60 @@
 function questao01() {
-  let nome = prompt("Nome: ");
-  let idade = prompt("Idade: ");
-  let cidade = prompt("Cidade: ");
+  let nome = document.getElementById("nome").value;
+  let idade = document.getElementById("idade").value;
+  let cidade = document.getElementById("cidade").value;
 
-  alert(`Seu nome é ${nome}, tem ${idade} e mora em ${cidade}.`);
+  let resposta = document.getElementById("resposta01");
+
+  resposta.textContent = `Seu nome é ${nome}, tem ${idade} e mora em ${cidade}.`;
 }
 
 function questao02() {
-  let valor = prompt("Valor do produto: R$ ");
+  let valor = document.getElementById("valor").value;
   let desconto = 0.2;
   let valor_final = valor - valor * desconto;
 
-  alert(
-    `Preço total do produto é R$${valor}, com 20% de desconto fica de R$${valor_final}.`
-  );
+  let resposta = document.getElementById("resposta02");
+
+  resposta.textContent = `Preço total do produto é R$${valor}, com 20% de desconto fica por R$${valor_final}.`;
 }
 
 function questao03() {
-  let celsius = prompt("Temperatura em Celsius: ");
+  let celsius = document.getElementById("temperatura").value;
   let convertido = (celsius * 9) / 5 + 32;
 
-  alert(`A temperatura de ${celsius}°C convertida fica de ${convertido}°F.`);
+  let resposta = document.getElementById("resposta03");
+
+  resposta.textContent = `A temperatura de ${celsius}°C convertida fica de ${convertido}°F.`;
 }
 
 function questao04() {
-  let base = Number(prompt("Base: "));
-  let altura = Number(prompt("Altura: "));
+  let base = Number(document.getElementById("base").value);
+  let altura = Number(document.getElementById("altura").value);
 
   let perimetro = base + base + altura + altura;
   let area = base * altura;
 
-  alert(`O perimetro é de ${perimetro} e a área ${area}`);
+  let resposta = document.getElementById("resposta04");
+
+  resposta.textContent = `O perimetro é de ${perimetro} e a área ${area}`;
 }
 
 function questao05() {
-  let ano_nascimento = Number(prompt("Ano de Nascimento: "));
+  let ano_nascimento = Number(document.getElementById("ano_nascimento").value);
   let ano_atual = Number(2025);
   let idade = ano_atual - ano_nascimento;
 
-  alert(`Você nasceu em ${ano_nascimento} e tem ${idade} anos de idade.`);
+  let resposta = document.getElementById("resposta05");
+
+  resposta.textContent = `Você nasceu em ${ano_nascimento} e tem ${idade} anos de idade.`;
 }
 
 function questao06() {
-  let idade = Number(prompt("Idade: "));
+  let idade = Number(document.getElementById("idade_atual").value);
   let ano_atual = Number(2025);
   let ano_nascimento = ano_atual - idade;
 
-  alert(`Você tem ${idade} anos e nasceu em ${ano_nascimento}.`);
+  let resposta = document.getElementById("resposta06");
+
+  resposta.textContent = `Você tem ${idade} anos e nasceu em ${ano_nascimento}.`;
 }
