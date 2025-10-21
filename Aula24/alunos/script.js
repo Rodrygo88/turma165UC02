d48b587e52426ee655c472fe47b2ad6510ff9b32
Rodrigo <2rodrigosilva2@gmail.com>
2025-10-20 21:25:52 -0300

    aula25 REACT

--- Aula24/alunos/script.js
+++ Aula24/alunos/script.js
@@ -1,22 +1,27 @@
-let tarefas = [];
-let input = document.querySelector("#tarefa");
+let tarefas = JSON.parse(localStorage.getItem("alunos")) || [];
+let aluno = document.querySelector("#aluno");
+let matricula = document.querySelector("#matricula");
 let lista = document.querySelector("#lista");
 let btnAdd = document.querySelector("#btnAdd");
-
 let indiceEdicao = null;
 
+mostrarTarefas();
+
 btnAdd.addEventListener("click", () => {
-    if(input.value === ""){
+    if(aluno.value === "" || matricula.value === ""){
         alert("Digite uma tarefa");
     }else if(indiceEdicao == null){
-        tarefas.push(input.value);
+        tarefas.push({"aluno": aluno.value, "matricula": matricula.value});
+
     } else{
-        tarefas[indiceEdicao] = input.value
+        tarefas[indiceEdicao] = aluno.value
         indiceEdicao = null
         btnAdd.textContent = "Adicionar"
+        
     }
-    input.value = "";
+    aluno.value = "";
     mostrarTarefas();
+    salvarLocalStorage();
 });
 
 
@@ -24,7 +29,7 @@ function mostrarTarefas(){
     lista.innerHTML = ""
     tarefas.forEach((t, i)=>{
         let li = document.createElement("li");
-        li.textContent = t;
+        li.textContent = `Aluno: ${t.aluno} - Matricula: ${t.matricula}`;
 
         let editar = document.createElement("button");
         editar.textContent = "Editar";
@@ -50,11 +55,18 @@ function apagar(i){
     }
     
     mostrarTarefas();
+    salvarLocalStorage();
 }
 
 function atualizar(i){
     btnAdd.textContent = "Atualizar";
-    input.value = tarefas[i];
+    aluno.value = tarefas[i].aluno;
+    matricula.value = tarefas[i].matricula;
     indiceEdicao = i;
+    
 
+}
+
+function salvarLocalStorage(){
+    localStorage.setItem("alunos", JSON.stringify(tarefas));
 }
