cf0ddecace0b62d9bed17facfe16981deb6282eb
Rodrigo <2rodrigosilva2@gmail.com>
2025-10-29 21:24:04 -0300

    aula29

--- Aula28/src/components/List/List.jsx
+++ Aula28/src/components/List/List.jsx
@@ -3,6 +3,9 @@ import style from "./List.module.css";
 
 export default function List({listaLivros = []}) {
 
+    <Extra>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus modi impedit a quisquam veniam mollitia quos illo quis est, sint laborum, eligendi recusandae consequatur? Ipsum culpa quidem a deserunt sed.</p>
+    </Extra>
 
     return (
         <>
